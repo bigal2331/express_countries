402aa248795f26d152129e0ef9deb32220076093
jnoriega <bigal2331@yahoo.com>
2017-10-03 17:37:57 +0000

    added some styles

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
 
 
 
